e875a51c4051643f14c54b17ba497828a62067ee
alonnastrong <110996560+alonnastrong@users.noreply.github.com>
2022-08-18 08:46:46 -0400

    Add files via upload
    Updated

---
+++ estee2/estee2/MyCollection.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollection.swift
+//  estee2
+//
+//  Created by Scholar on 8/17/22.
+//
+
+import UIKit
+
+class MyCollection: UICollectionViewCell {
+    
+    
+    @IBOutlet var myMakeupImage: UIImageView!
+    
+}
